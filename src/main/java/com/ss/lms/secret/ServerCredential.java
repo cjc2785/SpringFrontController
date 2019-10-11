cc040ca01c748bbd614b8ea644c1d4b6f9b908e9
Abdi <abdirahman.noor@smoothstack.com>
2019-10-11 14:22:05 -0400

    changed server credentials to static final

---
+++ src/main/java/com/ss/lms/secret/ServerCredential.java
@@ -0,0 +1,13 @@
+package com.ss.lms.secret;
+
+public class ServerCredential {
+
+    public static final String getServerPWord ()
+    {
+        return "Input Password";
+    }
+     public static final String  getServerUser()
+    {
+        return "Input User Login";
+    }
+}

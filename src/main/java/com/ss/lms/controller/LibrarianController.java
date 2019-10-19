89d9e49bf9c625cec6754c7693f54a25cb74cd22
tyler <tkatona2013@gmail.com>
2019-10-19 14:10:55 -0400

    POJO Cleanup

--- src/main/java/com/ss/lms/controller/LibrarianController.java
+++ src/main/java/com/ss/lms/controller/LibrarianController.java
@@ -2,18 +2,19 @@ package com.ss.lms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
-import com.ss.lms.model.BranchPOJO;
-import com.ss.lms.model.LibraryPOJO;
+import com.ss.lms.model.LibraryBranch;
+import com.ss.lms.model.BookCopies;
 import com.ss.lms.services.LibrarianService;
 
 
@@ -24,89 +25,45 @@ public class LibrarianController {
 	@Autowired
 	private LibrarianService library;
 
+	@ExceptionHandler(HttpClientErrorException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public @ResponseBody String handleResourceNotFound() { 
+		return "Resource not found";
+	}
+	
 	@GetMapping("branches")
-	@ResponseBody public ResponseEntity<?> getBranches(){
-		BranchPOJO[] branch = library.getBranches();
-		return new ResponseEntity<BranchPOJO[]>(branch, HttpStatus.OK);
+	@ResponseBody public LibraryBranch[] getBranches(){
+		return library.getBranches();
 	}
 	
 	@GetMapping("branch/{branchId}")
-	@ResponseBody public ResponseEntity<?> getBranchInfo(@PathVariable int branchId){
-		try {
-			BranchPOJO branch = library.getBranchInfo(branchId);
-			return new ResponseEntity<BranchPOJO>(branch, HttpStatus.OK);
-		}catch(HttpClientErrorException e){
-			switch(e.getStatusCode()) {
-			case NOT_FOUND:
-				return new ResponseEntity<String>("Branch not found", HttpStatus.NOT_FOUND);
-			default : 
-				return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}	
+	@ResponseBody public LibraryBranch getBranchInfo(@PathVariable int branchId){
+			return library.getBranchInfo(branchId);
 	}
 	
 	@PutMapping("branch/{branchId}")
-	@ResponseBody public ResponseEntity<?> updateBranch(@PathVariable int branchId, @RequestBody BranchPOJO updateBranch){
-		try {
+	@ResponseBody public LibraryBranch updateBranch(@PathVariable int branchId, @RequestBody LibraryBranch updateBranch){
 			library.save(updateBranch, branchId);
 			updateBranch.setBranchId(branchId);
-			return new ResponseEntity<BranchPOJO>(updateBranch, HttpStatus.OK);
-		}catch(HttpClientErrorException e){
-			switch(e.getStatusCode()) {
-			case NOT_FOUND:
-				return new ResponseEntity<String>("Branch not found", HttpStatus.NOT_FOUND);
-			default : 
-				return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}		
+			return updateBranch;
 	}
 	
 	@GetMapping("branch/{branchId}/books")
-	@ResponseBody public ResponseEntity<?> getBooks(@PathVariable int branchId) {
-		try {
-			 LibraryPOJO[] lib = library.getBooks(branchId);
-			 return new ResponseEntity<LibraryPOJO[]>(lib, HttpStatus.OK);
-		}catch(HttpClientErrorException e){
-			switch(e.getStatusCode()) {
-			case NOT_FOUND:
-				return new ResponseEntity<String>("Branch not found", HttpStatus.NOT_FOUND);
-			default : 
-				return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}	
+	@ResponseBody public BookCopies[] getBooks(@PathVariable int branchId) {
+			return library.getBooks(branchId);
 	}
 	
 	@GetMapping("branch/{branchId}/book/{bookId}")
-	@ResponseBody public ResponseEntity<?> getBookInfo(@PathVariable int branchId, @PathVariable int bookId) {
-		
-		try {
-			LibraryPOJO lib = library.getBookInfo(bookId, branchId);
-			return new ResponseEntity<LibraryPOJO>(lib, HttpStatus.OK);
-		}catch(HttpClientErrorException e){
-			switch(e.getStatusCode()) {
-			case NOT_FOUND:
-				return new ResponseEntity<String>("Book not found in this Branch", HttpStatus.NOT_FOUND);
-			default : 
-				return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}
+	@ResponseBody public BookCopies getBookInfo(@PathVariable int branchId, @PathVariable int bookId) {
+			return library.getBookInfo(bookId, branchId);
 	}
 	
 	@PutMapping("branch/{branchId}/book/{bookId}")
-	@ResponseBody public ResponseEntity<?> updateNoofCopies(@PathVariable int branchId, @PathVariable int bookId, @RequestBody LibraryPOJO newCopies) {
-		try {
-			LibraryPOJO book = library.getBookInfo(bookId, branchId);
+	@ResponseBody public BookCopies updateNoofCopies(@PathVariable int branchId, @PathVariable int bookId, @RequestBody BookCopies newCopies) {
+			BookCopies book = library.getBookInfo(bookId, branchId);
 			library.addCopies(bookId, branchId, newCopies);
 			int copies = book.getNoOfCopies() + newCopies.getNoOfCopies();
 			book.setNoOfCopies(copies);
-			return new ResponseEntity<LibraryPOJO>(book, HttpStatus.OK);
-		}catch(HttpClientErrorException e){
-			switch(e.getStatusCode()) {
-			case NOT_FOUND:
-				return new ResponseEntity<String>("Book not found in this Branch", HttpStatus.NOT_FOUND);
-			default : 
-				return new ResponseEntity<String>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
-			}
-		}
+			return book;
 	}
 }
